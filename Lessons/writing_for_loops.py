69e1d5016fb0645bdb0d3cc1c0001772dae10ec7
sophhuang <sohuang@unc.edu>
2024-10-09 11:30:04 -0400

    for_loops

--- Lessons/writing_for_loops.py
+++ Lessons/writing_for_loops.py
@@ -4,3 +4,9 @@ pets: list[str] = ["Louie", "Bo", "Bear"]
 
 for pet_name in pets:
     print(f"Good boy, {pet_name}!")
+
+
+names: list[str] = ["Alyssa", "Janet", "Vrinda"]
+#print each index name
+for idx in range(0,len(names)):
+    print(f"{names[idx]}: {names{idx}}")
